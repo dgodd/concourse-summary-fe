e0e716dbd8d3f44a89d15a27974b91f84d6fc16f
Dave Goddard <dave@goddard.id.au>
2017-05-25 08:59:30 -0400

    First step showing all jobs

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,22 +1,16 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <router-link v-for="job in jobs" :to="job.Job" target="_blank" class="outer running">
+      <div class="status">
+        <div class="succeeded" style="width: 100%;"></div>
+      </div>
+
+      <div class="inner">
+        <span><span style="font-size: 87.2727%;">{{job.Pipeline}}</span></span>
+        <span><span style="font-size: 61.1765%;">{{job.Job}}</span></span>
+        <span><span style="font-size: 93.8346%;">(0d) 00:44:57</span></span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -25,29 +19,45 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js PWA'
+      jobs: []
+    }
+  },
+  created () {
+    this.loadJobs()
+  },
+  methods: {
+    loadJobs () {
+      fetch('/jobs.json')
+        .then(resp => resp.json())
+        .then(jobs => { this.jobs = jobs; console.log(jobs) })
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.outer {display:block;width:300px;height:200px;color:white;background:#5C6C7D;position:relative;margin:4px;}
+.status {position:absolute;top:0;bottom:0;left:0;right:0;white-space:nowrap;overflow:hidden;text-align:left;}
+.paused_job, .aborted, .errored, .failed, .succeeded {display:inline-block;height:100%;margin:0;padding:0;float:left;}
+.paused_job {background:#3498DB;}
+.aborted {background:#8F4B2D;}
+.errored {background:#E67E21;}
+.failed {background:#E74C3C;}
+.succeeded {background:#2ECC71;}
+.paused {position:absolute;top:0;bottom:0;left:0;right:0;box-sizing:border-box;border:14px solid #2682D5;}
+.inner {position:absolute;top:0;bottom:0;left:0;right:0;text-align:center;text-decoration:none;white-space:nowrap;overflow:hidden;display:flex;justify-content:center;flex-direction:column;}
+.running .inner {height:100%;}
+ @-webkit-keyframes pulseBorder {
+  from { outline-offset: 0; }
+  to { outline-offset: 7px; }
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.running {
+  -webkit-animation-name: pulseBorder;
+  -webkit-animation-iteration-count: infinite;
+  -webkit-animation-timing-function: ease;
+  -webkit-animation-direction: alternate;
+  -webkit-animation-duration: 0.5s;
+  outline: solid 7px #F2C500;
+  outline-offset: 0;
 }
 </style>
